68df2b73a95cce88e87c47f14b98eb61331a346e
Никита Воропаев <voropaev.nd@phystech.edu>
2026-03-23 20:19:54 +0300

    change struct of weapon mechanics and Vec2.h

---
+++ src/math/Vec2.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <cmath>
+
+class Vec2 {
+  public:
+    Vec2() : x(0), y(0) {}
+    Vec2(double x, double y): x(x), y(y) {}
+    Vec2(double angle) {
+        x = std::cos(angle);
+        y = std::sin(angle);
+    }
+
+    // Сложение
+    Vec2 operator+(const Vec2& other) {
+        return Vec2(x + other.x, y + other.y);
+    }
+    // Вычитание
+    Vec2 operator-(const Vec2& other) {
+        return Vec2(x - other.x, y - other.y);
+    }
+    Vec2 operator*(double scalar) {
+        return Vec2(x * scalar, y * scalar);
+    }
+    double length() const {
+        return std::sqrt(x * x + y * y);
+    }
+    Vec2 normilized() const {
+        double len = length();
+        if (len == 0) return Vec2(0, 0);
+        return Vec2(x / len, y / len);
+    }
+    double angle() const {
+        return std::atan2(y, x);
+    }
+
+  private:
+    double x;
+    double y;
+};
