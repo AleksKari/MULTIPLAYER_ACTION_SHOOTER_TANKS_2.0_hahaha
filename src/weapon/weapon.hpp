29eac03d2184ff5a0b39121ca875de0710a77f41
Никита Воропаев <voropaev.nd@phystech.edu>
2026-03-24 16:24:34 +0300

    create virtual class weapon

--- src/weapon/weapon.hpp
+++ src/weapon/weapon.hpp
@@ -1,4 +1,9 @@
 #pragma once
+#include "../math/Vec2.h"
 
-
-
+class Weapon {
+  public:
+    virtual ~Weapon() = default;
+    virtual void shoot(const Vec2& pos, const Vec2& dir) = 0;
+    virtual double getcooldown() const = 0;
+};
