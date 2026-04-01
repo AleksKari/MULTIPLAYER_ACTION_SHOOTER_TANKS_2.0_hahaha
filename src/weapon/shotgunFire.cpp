c8f17726f0b80ae1720997babe19a4090c010026
Никита Воропаев <voropaev.nd@phystech.edu>
2026-04-01 19:41:50 +0300

    change velocity states of shotgun projectile

--- src/weapon/shotgunFire.cpp
+++ src/weapon/shotgunFire.cpp
@@ -6,7 +6,7 @@ void ShotgunFire::fire(const WeaponStates& states, const Vec2& pos, const Vec2& 
     double part_angel = states.spread_angle / states.projectile_per_shoot;
     double base_angel = dir.angle() - part_angel * (states.projectile_per_shoot / 2);
     for (int i = 0; i < states.projectile_per_shoot; ++i) {
-        Projectile p(pos, base_angel + i * part_angel, states.damage);
+        Projectile p(pos, (base_angel + i * part_angel) * states.projectile_speed, states.damage);
         //Добавление в игру
     }
 }
