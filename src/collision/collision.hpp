acc7ecc520aece3da5a4427ada5fe6e1544c584a
Никита Воропаев <voropaev.nd@phystech.edu>
2026-04-05 21:41:30 +0300

    add header of collision class

---
+++ src/collision/collision.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "../math/Vec2.h"
+#include "../map/map.hpp"
+#include "../../libraries/entity.hpp"
+#include "../../libraries/projectile.hpp"
+
+class Collision {
+  public:
+    static bool checkAABB(const Vec2& posA, const Vec2& dirA,
+                          const Vec2& posB, const Vec2& dirB);
+    static bool entity_tile(const Entity& entity, const Map& map);
+    static bool projectile_tile(const Projectile& proj, const Map& map);
+    static void resolve(Map& map);
+};
