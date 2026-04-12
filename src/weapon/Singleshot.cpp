7b94e941233ad9144fbf92bdfbde9ae1ee775c23
Алексей Карелин <karelin.ae@phystech.edu>
2026-04-12 15:59:45 +0300

    remake shooting for new texture

--- src/weapon/Singleshot.cpp
+++ src/weapon/Singleshot.cpp
@@ -3,7 +3,7 @@
 #include "../map/map.hpp"
 
 void SingleShot::shoot(Map& map, Player& owner, int damage) {
-  Vec2 center(owner.position.x + 63, owner.position.y + 39);
+  Vec2 center(owner.position.x + 16, owner.position.y + 16);
   Vec2 vel = owner.dir().normilized() * 400.0f;
   map.spawn_projectile(center, vel, damage, 6); // размер пули
 }
