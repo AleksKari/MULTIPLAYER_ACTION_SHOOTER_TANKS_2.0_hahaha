64cc0fd0a0e946702134cf4386b772b2c2cb72f9
Алексей Карелин <karelin.ae@phystech.edu>
2026-04-25 14:58:16 +0300

    remake textures and add empty tile

--- src/tile/EmptyTile.cpp
+++ src/tile/EmptyTile.cpp
@@ -4,7 +4,7 @@
 #include "../math/Vec2.h"
 
 EmptyTile::EmptyTile() {
-    texture_.loadFromFile("textures/stone.png"); //должна быть своя текстура
+    texture_.loadFromFile("textures/grass.png");
 }
 bool EmptyTile::is_empty() {return true;}
 void EmptyTile::draw(Renderer& renderer, Vec2 pos) {
