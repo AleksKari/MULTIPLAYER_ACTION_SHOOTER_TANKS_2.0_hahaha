a29ce228ed38e21727b3c626708149de0db89ef6
Алексей Карелин <karelin.ae@phystech.edu>
2026-04-27 16:11:34 +0300

    small rework in draw guns

--- src/weapon/Firemodes/Singleshot.cpp
+++ src/weapon/Firemodes/Singleshot.cpp
@@ -3,7 +3,6 @@
 #include "../../map/map.hpp"
 
 void SingleShot::shoot(Map& map, Player& owner, int damage) {
-  Vec2 center(owner.position.x + 16, owner.position.y + 16);
   Vec2 vel = owner.dir().normilized() * 400.0f;
-  map.spawn_projectile(center, vel, damage, 6); // размер пули
+  map.spawn_projectile(owner.position, vel, damage, 6); // размер пули
 }
