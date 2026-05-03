498f7845fcaf8e3fbefc0bc70038c2351a40fd9d
Алексей Карелин <karelin.ae@phystech.edu>
2026-05-03 18:07:54 +0300

    fix codestyle in collision

--- src/collision/collision.cpp
+++ src/collision/collision.cpp
@@ -2,92 +2,90 @@
 #include "map/map.hpp"
 #include <iostream>
 
-bool Collision::checkAABB(const Vec2& posA, const int sizeA,
-                          const Vec2& posB, const int sizeB) {
-  double dist = sqrt((posA.x - posB.x) * (posA.x - posB.x) + (posA.y - posB.y) * (posA.y - posB.y));
-  return (dist - ((sizeA + sizeB) / 2.0) > 0);
-}
 
-bool Collision::entity_tile(const Player& e, const Map& map) {
-  if (e.position.x < 0 || e.position.y < 0 ||
-      ((e.position.x + e.size) / 32) >= map.width_ ||
-      ((e.position.y + e.size) / 32) >= map.height_) { return true; }
-  int x0 = static_cast<int>(e.position.x) / 32;
-  int y0 = static_cast<int>(e.position.y) / 32;
-  int x1 = static_cast<int>(e.position.x + e.size - 1) / 32;
-  int y1 = static_cast<int>(e.position.y + e.size - 1) / 32;
-  std::cout << x0 << " " << y0 << " " << x1 << " " << y1 << "\n";
-  std::cout << map.tiles_.size() << " " << map.tiles_[0].size() << std::endl;
-  return (map.tiles_[x0][y0] && map.tiles_[x0][y0]->is_wall()) ||
-         (map.tiles_[x1][y0] && map.tiles_[x1][y0]->is_wall()) ||
-         (map.tiles_[x0][y1] && map.tiles_[x0][y1]->is_wall()) ||
-         (map.tiles_[x1][y1] && map.tiles_[x1][y1]->is_wall());
+bool Collision::entity_projectile(const Vec2& pos_projectile, const int size_projectile, const Vec2& pos_entity, const int size_entity = 32) {
+  double center_projectile_x = pos_projectile.x + (size_projectile / 2.0);
+  double center_projectile_y = pos_projectile.y + (size_projectile / 2.0);
+  double center_entity_x = pos_entity.x + (size_entity / 2.0);
+  double center_entity_y = pos_entity.y + (size_entity / 2.0);
+  double dist_x = center_projectile_x - center_entity_x;
+  double dist_y = center_projectile_y - center_entity_y;
+  double radius_projectile = size_projectile / 2.0;
+  double radius_entity = size_entity / 2.0;
+  double distance_between_projectile_and_entity = (dist_x * dist_x) + (dist_y * dist_y);
+  double minimum_distance_between_projectile_and_entity = (radius_projectile + radius_entity) * (radius_projectile + radius_entity);
+  return distance_between_projectile_and_entity < minimum_distance_between_projectile_and_entity;
 }
-
+// x0 x1 y0 y1 координаты в тайлах где находится плеер
+bool Collision::entity_tile (const Player& entity, const Map& map) {
+  if (entity.position.x < 0 || entity.position.y < 0 ||
+      ((entity.position.x + entity.size) / 32) >= map.width_ ||
+      ((entity.position.y + entity.size) / 32) >= map.height_) { return true; }
+  int x0 = static_cast<int>(entity.position.x) / 32;
+  int y0 = static_cast<int>(entity.position.y) / 32;
+  int x1 = static_cast<int>(entity.position.x + entity.size) / 32;
+  int y1 = static_cast<int>(entity.position.y + entity.size) / 32;
+  return (map.tiles_[x0][y0]->is_wall()) || (map.tiles_[x1][y0]->is_wall()) ||
+        (map.tiles_[x0][y1]->is_wall()) || (map.tiles_[x1][y1]->is_wall());
+}
+//x0 x1 y0 y1 также для пули
 bool Collision::projectile_tile(const Projectile& proj, const Map& map) {
   if (proj.position.x < 0 || proj.position.y < 0 ||
       proj.position.x + proj.size > map.width_ * 32 ||
       proj.position.y + proj.size > map.height_ * 32) { return true; }
   int x0 = static_cast<int>(proj.position.x) / 32;
   int y0 = static_cast<int>(proj.position.y) / 32;
-  int x1 = static_cast<int>(proj.position.x + proj.size - 1) / 32;
-  int y1 = static_cast<int>(proj.position.y + proj.size - 1) / 32;
-  return (map.tiles_[x0][y0] && map.tiles_[x0][y0]->is_wall()) ||
-         (map.tiles_[x1][y0] && map.tiles_[x1][y0]->is_wall()) ||
-         (map.tiles_[x0][y1] && map.tiles_[x0][y1]->is_wall()) ||
-         (map.tiles_[x1][y1] && map.tiles_[x1][y1]->is_wall());
+  int x1 = static_cast<int>(proj.position.x + proj.size) / 32;
+  int y1 = static_cast<int>(proj.position.y + proj.size) / 32;
+  return (map.tiles_[x0][y0] && map.tiles_[x0][y0]->is_wall()) || (map.tiles_[x1][y0]->is_wall()) ||
+        (map.tiles_[x0][y1] && map.tiles_[x0][y1]->is_wall()) ||(map.tiles_[x1][y1]->is_wall());
 }
 
 void Collision::resolve(Map& map) {
-  std::cout << "Collision start\n";
-  for (auto& p : map.projectiles_) {
-    if (!projectile_tile(*p, map)) continue;
-    if (p->hp_ > 1) {
-      if (p->lifetime() > 7) { p->kill(); continue; }
-      bool oob_x = p->position.x < 0 || p->position.x + p->size > map.width_ * 32;
-      bool oob_y = p->position.y < 0 || p->position.y + p->size > map.height_ * 32;
-      if (oob_x || oob_y) {
-        if (oob_x) p->velocity_.x = -p->velocity_.x;
-        if (oob_y) p->velocity_.y = -p->velocity_.y;
+  for (auto& projectile : map.projectiles_) {
+    if (!projectile_tile(*projectile, map)) continue;
+    if (projectile->hp_ > 1) {
+      if (projectile->lifetime() > 7) { projectile->kill(); continue; }
+      bool not_on_map_x = projectile->position.x < 0 || projectile->position.x + projectile->size > map.width_ * 32;
+      bool not_on_map_y = projectile->position.y < 0 || projectile->position.y + projectile->size > map.height_ * 32;
+      if (not_on_map_x || not_on_map_y) {
+        if (not_on_map_x) projectile->velocity_.x = -projectile->velocity_.x;
+        if (not_on_map_y) projectile->velocity_.y = -projectile->velocity_.y;
       } else {
-        int curr_x = (int)(p->position.x + p->size) / 32;
-        int curr_y = (int)(p->position.y + p->size) / 32;
-        int prev_x = (int)(p->prev_position_.x + p->size) / 32;
-        int prev_y = (int)(p->prev_position_.y + p->size) / 32;
-        if (curr_x != prev_x) p->velocity_.x = -p->velocity_.x;
-        if (curr_y != prev_y) p->velocity_.y = -p->velocity_.y;
-        if (curr_x == prev_x && curr_y == prev_y) {
-          p->velocity_.x = -p->velocity_.x;
-          p->velocity_.y = -p->velocity_.y;
+        int curr_projectile_x = static_cast<int>(projectile->position.x + projectile->size) / 32;
+        int curr_projectile_y = static_cast<int>(projectile->position.y + projectile->size) / 32;
+        int prev_projectile_x = static_cast<int>(projectile->prev_position_.x + projectile->size) / 32;
+        int prev_projectile_y = static_cast<int>(projectile->prev_position_.y + projectile->size) / 32;
+        if (curr_projectile_x != prev_projectile_x) projectile->velocity_.x = -projectile->velocity_.x;
+        else if (curr_projectile_y != prev_projectile_y) projectile->velocity_.y = -projectile->velocity_.y;
+        if (curr_projectile_x == prev_projectile_x && curr_projectile_y == prev_projectile_y) {
+          projectile->velocity_.x = -projectile->velocity_.x;
+          projectile->velocity_.y = -projectile->velocity_.y;
         }
       }
-
-      p->position = p->prev_position_;
-      p->position.x += p->velocity_.x > 0 ? 1 : -1;
-      p->position.y += p->velocity_.y > 0 ? 1 : -1;
-      p->take_damage(1);
+      projectile->position = projectile->prev_position_;
+      projectile->position.x += projectile->velocity_.x > 0 ? 1 : -1;
+      projectile->position.y += projectile->velocity_.y > 0 ? 1 : -1;
+      projectile->take_damage(1);
     } else {
-      p->kill();
+      projectile->kill();
     }
   }
-
-  for (auto& p : map.projectiles_) {
-    if (p->isDead()) continue;
-    if (p->lifetime() < 0.1) continue;
-    for (auto& e : map.entities_) {
-      if (e->isDead()) continue;
-      if (!checkAABB(p->position, p->size, e->position, e->size)) {
-        e->take_damage(p->damage);
-        p->kill();
+  for (auto& projectile : map.projectiles_) {
+    if (projectile->isDead()) continue;
+    if (projectile->lifetime() < 0.1) continue;
+    for (auto& entity : map.entities_) {
+      if (entity->isDead()) continue;
+      if (entity_projectile(projectile->position, projectile->size, entity->position, entity->size)) {
+        entity->take_damage(projectile->damage);
+        projectile->kill();
         break;
       }
     }
   }
-
-  for (auto& e : map.entities_) {
-    if (entity_tile(*e, map)) {
-      std::cout << "NU\n";
-      e->on_wall_collision();
+  for (auto& entity : map.entities_) {
+    if (entity_tile(*entity, map)) {
+      entity->on_wall_collision();
     }
   }
 }
--- src/collision/collision.hpp
+++ src/collision/collision.hpp
@@ -8,7 +8,7 @@ class Map; //forward declaration
 
 class Collision {
   public:
-    static bool checkAABB(const Vec2& posA, const int dirA,
+    static bool entity_projectile(const Vec2& posA, const int dirA,
                           const Vec2& posB, const int dirB);
     static bool entity_tile(const Player& entity, const Map& map);
     static bool projectile_tile(const Projectile& proj, const Map& map);
