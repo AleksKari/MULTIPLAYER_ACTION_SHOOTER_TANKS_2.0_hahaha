76d20b2b1b9ae379d40e9a3b6606c47f3124387c
7artem <kalinovskii.ae@phystech.edu>
2026-05-12 00:46:32 +0300

    Add Network Client Class

---
+++ src/network/ClientNetwork.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include <SFML/Network.hpp>
+#include "Protocol.hpp"
+#include <string>
+#include <iostream>
+
+class ClientNetwork {
+public:
+    ClientNetwork() {
+        socket.setBlocking(false);
+    }
+
+    bool connectToLobby(const std::string& ip, unsigned short port, const std::string& lobbyCode) {
+        serverIp = sf::IpAddress(ip);
+        serverPort = port;
+
+        sf::Packet packet;
+        packet << PacketType::JoinLobby << lobbyCode;
+        
+        return socket.send(packet, serverIp, serverPort) == sf::Socket::Done;
+    }
+
+    void sendInput(float moveX, float moveY, bool isShooting) {
+        sf::Packet packet;
+        packet << PacketType::PlayerInput << moveX << moveY << isShooting;
+        socket.send(packet, serverIp, serverPort);
+    }
+
+    void receiveUpdates() {
+        sf::Packet packet;
+        sf::IpAddress sender;
+        unsigned short port;
+
+        while (socket.receive(packet, sender, port) == sf::Socket::Done) {
+            if (sender != serverIp) continue; // Игнорируем левые пакеты
+
+            PacketType type;
+            if (packet >> type) {
+                processPacket(type, packet);
+            }
+        }
+    }
+
+private:
+    void processPacket(PacketType type, sf::Packet& packet) {
+        if (type == PacketType::GameState) {
+            uint32_t entityCount;
+            packet >> entityCount;
+        }
+    }
+
+    sf::UdpSocket socket;
+    sf::IpAddress serverIp;
+    unsigned short serverPort;
+};
